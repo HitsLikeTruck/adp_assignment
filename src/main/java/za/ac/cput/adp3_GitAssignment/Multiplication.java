5b386119517ddbcc76dded7168034d4ebae4ccb5
DinelleK219089302 <81468993+DinelleK219089302@users.noreply.github.com>
2021-04-01 20:41:20 +0200

    Update Multiplication.java

--- src/main/java/za/ac/cput/adp3_GitAssignment/Multiplication.java
+++ src/main/java/za/ac/cput/adp3_GitAssignment/Multiplication.java
@@ -1,11 +1,18 @@
 package za.ac.cput.adp3_GitAssignment;
 
 /**
- *
- * small multiplication program
- *
+ * 1 April 2021
+ * Small Multiplication Program
+ * @author Dinelle Kotzé
+ * 219089302
  * **/
 
 
-public class Multiplication {
+public class Multiplication
+{
+    public int multiplication(int x, int y)
+    {
+        return x * y;
+        //return 0 fails test
+    }
 }
